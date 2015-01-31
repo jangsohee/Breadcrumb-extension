0b14abf32d68f5f205e29599fcae4678b9dce1d4
SangBong Rhee <chzmo4@gmail.com>
2015-02-01 00:44:10 +0900

    스크래핑 후 Background로 전달 구현
    Content Script에서 페이지 내용을 스크래핑 한 후 Background로 전달해
    주는 과정을 구현했음. 아직은 string 형태이지만 JSON화 한 후 약간의
    가공을 할 예정.
    
    이슈: 없음
    구현: 스크래핑

--- Extension/background.js
+++ Extension/background.js
@@ -1,4 +1,4 @@
-function eb() {
+function makeUIPopup() {
     var flags = {
         url: window.chrome.extension.getURL("main_ui.html"),
         type: "popup",
@@ -11,6 +11,33 @@ function eb() {
     window.chrome.windows.create(flags);
 }
 
+
+// 여기서는 왜 DOMContentLoaded가 감지 되는가?
 document.addEventListener('DOMContentLoaded', function() {
-  eb();
+    makeUIPopup();
 });
+
+
+function copyToClipboard( text ){
+    var copyDiv = document.createElement('div');
+    copyDiv.contentEditable = true;
+    document.body.appendChild(copyDiv);
+    copyDiv.innerText = text;
+    copyDiv.unselectable = "off";
+    copyDiv.focus();
+    document.execCommand('SelectAll');
+    document.execCommand("Copy", false, null);
+    document.body.removeChild(copyDiv);
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(sender.tab ?
+        "from a content script:" + sender.tab.url :
+            "from the extension");
+        if (request.data)
+        {
+            copyToClipboard(request.data);
+            sendResponse({message: "I've finished to receive your data."});
+        }
+    });
--- Extension/content.js
+++ Extension/content.js
@@ -3,7 +3,61 @@
  */
 
 
+/*
+    a 태그 카운터 부분
+    TODO: a 태그 document 오브젝트에서 받아서 카운팅 해보기
+ */
 var start_anchors = document.getElementsByTagName('a').length;
 
-var end_anchors;
-console.log(anchors);
+var end_anchors = document.anchors;
+console.log(start_anchors + " vs " + end_anchors);
+
+
+/*
+    HTML 소스 가져오는 부분
+
+    TODO: 현재 outerText로 텍스트만 가져올 때 전부 다 제대로 못가져오는 현상이 있다.
+ */
+function getHTML(){
+    // return document.documentElement.outerHTML;
+    return document.documentElement.outerText;
+}
+
+$( document ).ready(function() {
+    // 소스 전달부
+    chrome.runtime.sendMessage({data: getHTML()}, function(response) {
+        console.log(response.message);
+    });
+    console.log("getHTML() is fired");
+});
+
+
+
+/*
+왜 DOMContentLoaded 이벤트는 감지가 안될까??
+document.addEventListener('DOMContentLoaded', function() {
+    chrome.runtime.sendMessage({data: getHTML()}, function(response) {
+        console.log(response.message + Date.getTime());
+    });
+    console.log("getHTML() is fired at " + Date.getTime());
+}, false);
+ */
+
+/*
+나중을 위해 남겨둠
+chrome.extension.onRequest.addListener(
+    function(request, sender, sendResponse) {
+        if(request.method == "getText"){
+            sendResponse({data: getHTML(), method: "getText"}); //same as innerText
+        }
+        else if(request.method == "setAutoOn"){
+            copyToClipboard(getHTML());
+            sendResponse({data: "Auto copy mode is on.", method: "setAutoOn"});
+        }
+        else if(request.method == "setAutoOff"){
+            copyToClipboard(getHTML());
+            sendResponse({data: "Auto copy mode is off.", method: "setAutoOff"});
+        }
+    }
+);
+ */
