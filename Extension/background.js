69f90f2e89466bf8dc2c6763ec82721e1829878e
SangBong Rhee <chzmo4@gmail.com>
2015-02-01 02:17:40 +0900

    Tool 버전 시작
    페이지 텍스트를 자동으로 클립보드로 복사해 준다.

--- Extension/background.js
+++ Extension/background.js
@@ -1,21 +1,21 @@
-function makeUIPopup() {
-    var flags = {
-        url: window.chrome.extension.getURL("main_ui.html"),
-        type: "popup",
-        left: 1,
-        top: 1,
-        width: 400,
-        height: window.screen.availHeight - 1 - 1,
-        focused: true
-    };
-    window.chrome.windows.create(flags);
-}
-
-
-// 여기서는 왜 DOMContentLoaded가 감지 되는가?
-document.addEventListener('DOMContentLoaded', function() {
-    makeUIPopup();
-});
+//function makeUIPopup() {
+//    var flags = {
+//        url: window.chrome.extension.getURL("main_ui.html"),
+//        type: "popup",
+//        left: 1,
+//        top: 1,
+//        width: 400,
+//        height: window.screen.availHeight - 1 - 1,
+//        focused: true
+//    };
+//    window.chrome.windows.create(flags);
+//}
+//
+//
+//// 여기서는 왜 DOMContentLoaded가 감지 되는가?
+//document.addEventListener('DOMContentLoaded', function() {
+//    makeUIPopup();
+//});
 
 
 function copyToClipboard( text ){
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Breadcrumb-extension
-SSM 25-1 Breadcrumb chrome extension
+# Breadcrumb-extension-tool
+SSM 25-1 Breadcrumb chrome extension-tool version for KSI
+
+확장 프로그램을 설치하면 페이지 방문시 자동으로 페이지 텍스트가 클립보드에 저장됨.
+클립보드로 저장되는 시점 : 페이지 로딩이 끝난 후 1초 이내
+
+최초 로딩만 감지하기 때문에 페이지 내에서 동적으로 바뀌는 부분은 가져올 수 없다.
+차후 해결 예정
