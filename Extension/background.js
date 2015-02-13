c72f52ac7b708210fe124c36ec4d7ff7405ea1ee
SangBong Rhee <chzmo4@gmail.com>
2015-02-13 09:20:15 +0900

    설정파일 변경 및 수정

--- Extension/background.js
+++ Extension/background.js
@@ -12,19 +12,6 @@ function makeUIPopup() {
     window.chrome.windows.create(flags);
 }
 
-function makeTestUIPopup() {
-    var flags = {
-        url: window.chrome.extension.getURL("test.html"),
-        type: "popup",
-        left: 401,
-        top: 1,
-        width: 800,
-        height: window.screen.availHeight - 1 - 1,
-        focused: true
-    };
-
-    window.chrome.windows.create(flags);
-}
 
 
 // 여기서는 왜 DOMContentLoaded가 감지 되는가?
@@ -36,7 +23,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-    alert("Test");
     chrome.windows.getAll({'populate': true}, function (windows) {
         var i, j;
         for (i = 0; i < windows.length; i++) {
